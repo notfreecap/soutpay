4a901c3324b7930393c3928c99862bcf5584a656
notfreecap <andresrenew@gmail.com>
2022-05-29 18:28:10 -0500

    using feing

--- payment-service/src/main/java/com/soutpay/payment/client/ProductClient.java
+++ payment-service/src/main/java/com/soutpay/payment/client/ProductClient.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "inventory-servce")
-@RequestMapping("/products")
+//@FeignClient(name = "localhost:8091/inventory-servce", path = "/products")
+// sin load balancer
+@FeignClient(name = "inventory-servce", url = "localhost:8091", path = "/products")
 public interface ProductClient {
 
     @GetMapping("/{id}")

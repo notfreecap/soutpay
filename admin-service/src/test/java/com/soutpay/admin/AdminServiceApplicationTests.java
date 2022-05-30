db76bac10570451ceef616d3a490e3fa2d67922e
notfreecap <andresrenew@gmail.com>
2022-05-30 10:14:45 -0500

    spring admin

---
+++ admin-service/src/test/java/com/soutpay/admin/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soutpay.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
